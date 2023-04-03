6cabfda36b4c6c5fd7e81a63942ebc69601aba52
bencevoros <bencee.voros001@gmail.com>
2023-04-03 12:06:07 +0200

    Install graphql and material-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    GraphQLModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
